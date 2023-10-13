3d2b311f428393b85d9ad0bc1c47a1778842e0d5
momo <mhamedbattioui@gmail.com>
2023-10-13 11:13:02 -0400

    place

--- code/main.cpp
+++ code/main.cpp
@@ -4,7 +4,7 @@
 int main() {
 	std::cout << "Hello World!" << std::endl;
 
-	//ls, done/done(all), remove/remove(all), make, change, update(time), priority, rank, sous-todo ex math exam devoir, count, head, tail
+	//ls, done/done(all), remove/remove(all), make, change, update(time), priority, rank, sous-todo ex math exam devoir, count, head, tail, localisation
 
 	return 0;
 }
--- code/object.h
+++ code/object.h
@@ -5,6 +5,7 @@ class todo {
 		int priorite;
 		int envie;
 		bool done = false;
+		std::string place;
 
 };
 
